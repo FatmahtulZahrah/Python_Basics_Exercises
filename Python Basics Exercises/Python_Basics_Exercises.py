335bb2d2cc99648db784916008e54687da437507
Fatima tul Zahra <zahratulfatima78@gmail.com>
2024-11-22 20:56:07 +0500

    first commit

---
+++ Python Basics Exercises/Python_Basics_Exercises.py
@@ -0,0 +1,120 @@
+# 1. Simple Message
+
+message = "How are you?"
+print(message)
+
+# 2. Simple Messages
+
+message = "How are you?"
+print(message)
+message = "I hope u are doing well!"
+print(message)
+
+# 3. Personal Message
+
+Elon_Musk = """"“Hello Musk, would you like to learn some
+Python today?”"""
+print(Elon_Musk)
+
+# 4. Name Cases
+
+Person_name = "ELON MUSK"
+print (Person_name.lower()) 
+print (Person_name.title()) 
+print (Person_name.upper()) 
+
+# 5. Famous Quote
+
+Famous_Quote = '"have the courage to follow your heart and intuition. they somehow know what you truly want to become."'
+print(f"Steve Jobs Once Said, \n\t {Famous_Quote.upper()}")
+
+# 6. Famous Quote 2
+
+famous_person = "elon musk"
+message = f"Hello {famous_person.title()},  Now on which project you has to be working to surprise the world😮 "
+print(message)
+
+# 7. Stripping Names
+ 
+person_name = " Elon\nMusk "
+print(person_name)
+new_name = person_name.lstrip()
+print(new_name)
+new_name = person_name.rstrip()
+print(new_name)
+new_name = person_name.strip()
+print(new_name)
+
+# 8. Variable Sum
+
+x = 5
+y = 10
+z = 15
+a = x + y + z
+print(a)
+# or
+x, y, z =  5  , 10 , 15
+print(x+y+z)
+
+# 9. Variable Swap
+
+a, b = 5, 10
+print(f"before swap a = {a} b = {b}")
+
+a, b = b , a
+print(f"after swapping a = {a} b = {b}")
+
+# 10. Favorite Color
+favorite_color = "teal"
+print(favorite_color)
+color_variable = favorite_color
+print(color_variable)
+
+# 11. Changing Pet Name
+pet_name = "buddy"
+pet_name ="max"
+print(pet_name)
+
+# 12. Observing Name Error
+
+Sun_behave = "Sunshine"
+print(Sun_behave)
+# print(Sun)   Error Occurred
+
+# 13. Reassigning Score
+
+score = 100
+print(score)
+score = 150
+print(score)
+
+# 14. City Name
+
+city_name = "InterLaken"
+print("Wish to travel",city_name)
+
+# 15. Title Case Text
+
+text = "python programming"
+print(text.title())
+
+# 16. Lowercase Conversion
+
+lowercase_letters = "Hello, World!"
+print(lowercase_letters.lower())
+
+# 17. Uppercase Conversion
+
+uppercase_letters = "Hello, hi, SALAaM"
+print(uppercase_letters)
+
+# 18. Current Temperature
+
+temperature = 25
+print(f"The current temperature is {temperature} degrees")
+
+# 19. Printing a Poem
+
+poem = "'Untitled by Ashley' \nmay all your wishes come true tonight,\nand the nightmares of the past days vanish\nat the first break of dawn when light emerges,\nand there is a division between the darkness\nbeckoning for the rays of light to shine" 
+print(poem)
+
